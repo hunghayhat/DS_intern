0fee991f4aba360e87b009d794271bf68597856a
hunghayhat <diegocoshung19@gmail.com>
2024-06-11 16:07:30 +0700

    add database migration

--- timesheet-system/database/migrations/2024_06_11_081809_create_timesheets_table.php
+++ timesheet-system/database/migrations/2024_06_11_081809_create_timesheets_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('timesheets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->date('date');
+            $table->date('check_in_time');
+            $table->date('check_out_time');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('timesheets');
     }
 };
