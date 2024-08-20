b15bde71a9cc01cc37b589639b2c987a3c8b1ba7
hunghayhat <diegocoshung19@gmail.com>
2024-08-20 15:24:30 +0700

    fix bù chat feature

--- product/learning-project/bootstrap/providers.php
+++ product/learning-project/bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
-    Illuminate\Broadcasting\BroadcastServiceProvider::class,
+    // Illuminate\Broadcasting\BroadcastServiceProvider::class,
 
 ];
--- product/learning-project/resources/js/bootstrap.js
+++ product/learning-project/resources/js/bootstrap.js
@@ -9,11 +9,8 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: "pusher",
-    key: process.env.MIX_PUSHER_APP_KEY || '017e5baf7ec4223db9d0', // Sử dụng giá trị mặc định hoặc từ biến môi trường
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER || 'ap3', // Sử dụng giá trị mặc định hoặc từ biến môi trường
-    wsHost: process.env.MIX_PUSHER_HOST || 'ws-pusher.pusher.com', // Sử dụng giá trị mặc định hoặc từ biến môi trường
-    wsPort: process.env.MIX_PUSHER_PORT || 80, // Sử dụng giá trị mặc định hoặc từ biến môi trường
-    wssPort: process.env.MIX_PUSHER_PORT || 443, // Sử dụng giá trị mặc định hoặc từ biến môi trường
-    forceTLS: (process.env.MIX_PUSHER_SCHEME || 'https') === 'https', // Sử dụng giá trị mặc định hoặc từ biến môi trường
+    key: import.meta.env.VITE_PUSHER_APP_KEY || '017e5baf7ec4223db9d0', // Sử dụng giá trị mặc định hoặc từ biến môi trường
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER || 'ap3', // Sử dụng giá trị mặc định hoặc từ biến môi trường
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME || 'https') === 'https', // Sử dụng giá trị mặc định hoặc từ biến môi trường
     enabledTransports: ["ws", "wss"],
 });
